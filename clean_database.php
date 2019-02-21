43fc78e9c7f8df3c50b4af1bd42eba9522ab6d40
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-02-20 23:50:44 -0800

    added loading icon

--- clean_database.php
+++ clean_database.php
@@ -3,17 +3,16 @@
 require_once('mysql_connect.php');
 
 $query = "TRUNCATE TABLE students;";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (1, 'Lebron James', 'Negotiations', 100);";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (2, 'Anthony Davis', 'Math', 80);";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (3, 'Kyle Kuzma', 'Science', 99);";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (4, 'Lonzo Ball', 'Communication', 70);";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (5, 'Dwayne Wade', 'English', 95);";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (6, 'Joel Embiid', 'Drama', 100);";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (7, 'Magic Johnson', 'PE', 100);";
+$query .= "INSERT INTO students (ID, student, course, grade) VALUES (1, 'Jaytrin Apinchapong', 'Negotiations', 100);";
+$query .= "INSERT INTO students (ID, student, course, grade) VALUES (2, 'Perry Tran', 'Math', 80);";
+$query .= "INSERT INTO students (ID, student, course, grade) VALUES (3, 'Mia Tran', 'Science', 99);";
+$query .= "INSERT INTO students (ID, student, course, grade) VALUES (4, 'Soora Choi', 'Communication', 70);";
+$query .= "INSERT INTO students (ID, student, course, grade) VALUES (5, 'Dwayne Johnson', 'English', 95);";
+$query .= "INSERT INTO students (ID, student, course, grade) VALUES (6, 'Steve Jobs', 'Drama', 100);";
+$query .= "INSERT INTO students (ID, student, course, grade) VALUES (7, 'Bill Gates', 'PE', 100);";
 $query .= "INSERT INTO students (ID, student, course, grade) VALUES (8, 'Michael Jordan', 'Environmental Science', 50);";
 $query .= "INSERT INTO students (ID, student, course, grade) VALUES (9, 'Ernie Johnson', 'English', 100);";
 $query .= "INSERT INTO students (ID, student, course, grade) VALUES (10, 'Charles Barkley', 'Cross Country', 20);";
-$query .= "INSERT INTO students (ID, student, course, grade) VALUES (11, 'Jason Taytum', 'Basketball', 85);";
 
 if(mysqli_multi_query($conn,$query)){
     $output['message'] = 'Database successfully reset.';
